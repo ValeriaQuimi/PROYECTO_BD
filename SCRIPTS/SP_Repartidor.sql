f687ad2ac1444afce3fdab3a690d41574274e89f
ValeriaQuimi <vdquimi@espol.edu.ec>
2025-08-21 17:13:24 -0500

    SP Para Repartidores

---
+++ SCRIPTS/SP_Repartidor.sql
@@ -0,0 +1,112 @@
+USE gemaplusdb; 
+
+-- SP para repartidor
+
+DELIMITER //
+
+CREATE PROCEDURE insertarRepartidor(
+    IN p_nombre VARCHAR(100),
+    IN p_telefono INT
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Error al insertar repartidor';
+    END;
+
+    START TRANSACTION;
+    
+    IF p_telefono IS NULL OR p_telefono <= 0 THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'El teléfono debe ser mayor a 0';
+    END IF;
+
+    INSERT INTO Repartidor(nombre, telefono)
+    VALUES (p_nombre, p_telefono);
+
+    COMMIT;
+END//
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE actualizarRepartidor(
+    IN p_id INT,
+    IN p_nuevoTelefono INT
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Error al actualizar repartidor';
+    END;
+
+    START TRANSACTION;
+
+    IF NOT EXISTS (SELECT 1 FROM Repartidor WHERE idRepartidor = p_id) THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Repartidor no encontrado';
+    END IF;
+
+    UPDATE Repartidor
+    SET telefono = p_nuevoTelefono
+    WHERE idRepartidor = p_id;
+
+    COMMIT;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE eliminarRepartidor(
+    IN p_id INT
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Error al eliminar repartidor';
+    END;
+
+    START TRANSACTION;
+    
+    IF NOT EXISTS (SELECT 1 FROM Repartidor WHERE idRepartidor = p_id) THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Repartidor no encontrado';
+    END IF;
+
+    DELETE FROM Repartidor WHERE idRepartidor = p_id;
+
+    COMMIT;
+END$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE consultarRepartidores()
+BEGIN
+
+ DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error al listar repartidores';
+    END;
+    START TRANSACTION;
+    SELECT idRepartidor, nombre, telefono
+    FROM Repartidor;
+    
+    COMMIT;
+END$$
+
+DELIMITER ;
+
+
