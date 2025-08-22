f3818ac47a2dfc4983fa9b41fe9dfab49fe6cbb4
Danna Urdanigo <dannninita@gmail.com>
2025-08-21 23:44:23 -0500

    Update segundo usuario.sql

--- SCRIPTS/usuarios.sql
+++ SCRIPTS/usuarios.sql
@@ -10,3 +10,10 @@ GRANT SELECT, INSERT ON gemaplusdb.Producto TO 'est1'@'localhost';
 GRANT SELECT ON gemaplusdb.reporteCategoriaMasVendida TO 'est1'@'localhost';
 
 --crear el segundo usuario
+CREATE USER 'est2'@'localhost' IDENTIFIED BY '2025bd2pa';
+
+-- Darle por lo menos 2 permisos 
+GRANT EXECUTE ON PROCEDURE gemaplusdb.insertarCliente TO 'est2'@'localhost';
+GRANT SELECT, UPDATE ON gemaplusdb.Pedido TO 'est2'@'localhost';
+
+GRANT SELECT ON gemaplusdb.reporteHistorialDeComprasDeClientes TO 'est2'@'localhost';
