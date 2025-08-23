35fa66f0674ce384266f42021bbd1cedebb764fc
Danna Urdanigo <dannninita@gmail.com>
2025-08-23 09:36:05 -0500

    Update indices.sql

--- SCRIPTS/indices.sql
+++ SCRIPTS/indices.sql
@@ -5,3 +5,7 @@ USE gemaplusdb;
 CREATE INDEX idx_pagosPorFecha ON pago(fechaPago);
 
 CREATE INDEX idx_metodosPagoPorFecha ON pago(fechaPago,metodoPago);
+
+CREATE INDEX idx_pedidosPorCliente ON Pedido(idCliente);
+
+CREATE INDEX idx_productosPorCategoria ON Producto(idCat);
